0ea966dde054ddefa1d8e5863678f11f117aca6c
qinjiyuan <jiyuan.qin@foxmail.com>
2020-01-03 11:47:39 +0800

    Initial commit

---
+++ src/main/java/top/mixedinfos/PasswordApplication.java
@@ -0,0 +1,13 @@
+package top.mixedinfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordApplication.class, args);
+    }
+
+}
